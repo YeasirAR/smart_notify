b6fa3028a5196038c0007f10d88c8cab8de8b311
Yeasir Arafat <yeasirar@gmail.com>
2022-12-11 14:15:52 +0600

    ListView Template

---
+++ lib/alarm_info.dart
@@ -0,0 +1,6 @@
+class AlarmInfo {
+  String title;
+  bool isLocation;
+  String date_location;
+  AlarmInfo(this.title, this.isLocation, this.date_location);
+}
---
+++ lib/reminder_info.dart
@@ -0,0 +1,6 @@
+class ReminderInfo {
+  String title;
+  bool isLocation;
+  String date_location;
+  ReminderInfo(this.title, this.isLocation, this.date_location);
+}
