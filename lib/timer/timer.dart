4bd29fcc757af5fa7c0e3e7c7dfb3b2b0b3aef9e
mdnayanmolla1 <120439250+mdnayanmolla1@users.noreply.github.com>
2022-12-20 07:24:01 +0600

    Timer Functionality Ends

--- lib/timer/timer.dart
+++ lib/timer/timer.dart
@@ -77,9 +77,81 @@ class _TimerState extends State<Timer> with TickerProviderStateMixin {
                     value: progress,
                     strokeWidth: 6,
                   ),
-                )
-              )
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isDismissed) {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Container(
+                          height: 300,
+                          child: CupertinoTimerPicker(
+                            mode: CupertinoTimerPickerMode.hms,
+                            
+                            initialTimerDuration: controller.duration!,
+                            onTimerDurationChanged: (time) {
+                              setState(() {
+                                controller.duration = time;
+                              });
+                            },
+                          )
+                          ,
+                        ),
+                      );
+                    }
+                  },
+                  child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, child) => Text(
+                      countText,
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isAnimating) {
+                      controller.stop();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      controller.reverse(
+                          from: controller.value == 0 ? 1.0 : controller.value);
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
+                  },
+                  child: RoundButton(
+                    icon: isPlaying == true ? Icons.pause : Icons.play_arrow,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    controller.reset();
+                    setState(() {
+                      isPlaying = false;
+                    });
+                  },
+                  child: RoundButton(
+                    icon: Icons.stop,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
